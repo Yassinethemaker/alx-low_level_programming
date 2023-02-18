2ead0d1ef859b27c9dbec277f96a93c8d4ec2b83
Yassine <yassine.aitkeltouma@gmail.com>
2023-02-17 18:15:43 -0800

    Now betty becomes my friend

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
