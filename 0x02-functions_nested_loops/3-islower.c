86ce6ff655346c1a094f45b432e99ad5e68dbaab
Yassine <yassine.aitkeltouma@gmail.com>
2023-02-22 18:47:11 -0800

    lowercase task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: 1 if c is lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
